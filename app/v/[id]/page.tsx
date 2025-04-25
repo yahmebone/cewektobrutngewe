4394d5e380079952269be5faafdd3a607c227a07
yahmebone <yahmebone@gmail.com>
2025-04-26 06:45:22 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -159,12 +159,27 @@ export default async function Video({ params }: PageProps) {
                         {file.title}
                     </CardTitle>
                 </CardHeader>
-            <p>{file.title} di {SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Xpanas Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma Viral Video Bokep Indo Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma Jepang Jav Barat Simontok hub sotwe olmek avtube pijat pure gudang pemerkosaan rumah tobrut inggris ngintip vcs binor yandex update remaja {SITENAME} wiki raja bokeptube full porno seponga simontok {file.title} <center><iframe title='ads' allowFullScreen={true} frameBorder={0} height='250' scrolling='no' src='https://ceriguna.blogspot.com' width='300'></iframe></center> playbokep indobokep xpanasonline indoh janda streaming jepang barat korea japan jav cina japanese china rusia arab india thailand hd anime hentai cewektobrutngewe gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor {SITENAME} remaja yandex update perselingkuhan wiki raja full com porno indoh Hotbabes Big Tits Family Freeporn Ass Naked Celebs Nude Cam Hot Videos Fucking Free Porno Adult Movies Mom Freesex Nudelive Cams Women Stepmom Hotwife Bigtits Melons Tube Huge Tits Pornos Film x Titshits Pornofilme Nice Natural Fuq Girls Teen Sex Pornstars Tube Dick Jihad</p>
-            <center><iframe title='ads' allowFullScreen={true} frameBorder={0} height='250' scrolling='no' src='https://ceriguna3.blogspot.com' width='300'></iframe></center>
+            <p>{file.title} di {SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Xpanas Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma Viral Video Bokep Indo Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma Jepang Jav Barat Simontok hub sotwe olmek avtube pijat pure gudang pemerkosaan rumah tobrut inggris ngintip vcs binor yandex update remaja {SITENAME} wiki raja bokeptube full porno seponga simontok {file.title} <center><script
+      dangerouslySetInnerHTML={{
+         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1025826})`,
+      }}
+   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1025826" data-width="300" data-height="262"></ins>
+</center> playbokep indobokep xpanasonline indoh janda streaming jepang barat korea japan jav cina japanese china rusia arab india thailand hd anime hentai smpsangecolmek gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor {SITENAME} remaja yandex update perselingkuhan wiki raja full com porno indoh Hotbabes Big Tits Family Freeporn Ass Naked Celebs Nude Cam Hot Videos Fucking Free Porno Adult Movies Mom Freesex Nudelive Cams Women Stepmom Hotwife Bigtits Melons Tube Huge Tits Pornos Film x Titshits Pornofilme Nice Natural Fuq Girls Teen Sex Pornstars Tube Dick Jihad</p>
+            <center><script
+      dangerouslySetInnerHTML={{
+         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1025827})`,
+      }}
+   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1025827" data-width="300" data-height="262"></ins>
+</center>
 </Card>
             <h2 className="text-2xl font-bold text-center my-4">
                 Related Video {file.title}
-            </h2><center><iframe title='ads' allowFullScreen={true} frameBorder={0} height='50' scrolling='no' src='https://ceriguna2.blogspot.com' width='300'></iframe></center>
+            </h2><center><script
+      dangerouslySetInnerHTML={{
+         __html: `(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':1025828})`,
+      }}
+   /><Script data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"/><ins id="1025828" data-width="300" data-height="262"></ins>
+</center>
             <SearchCardList query={file.title.split(" ")[3]} />
 <Script src="https://js.juicyads.com/jp.php?c=947403z2v256s2w2x2f4x2c4&u=http%3A%2F%2Fwww.juicyads.rocks"/>
         </div>
